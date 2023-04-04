4bdd1c5fdc3bb885a1932f328283151a5304b059
TranDangKhoi <baits16@gmail.com>
2023-04-04 18:52:37 +0700

    :+1: Update profile update code

---
+++ RailwayReservationAPI/RailwayReservationAPI/Controllers/ProfileController.cs
@@ -0,0 +1,72 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RailwayReservationAPI.Data;
+using RailwayReservationAPI.Models;
+using RailwayReservationAPI.Models.Dto;
+using System.Net;
+
+namespace RailwayReservationAPI.Controllers
+{
+    [Route("api/profile")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private string secretKey;
+        public ProfileController(ApplicationDbContext db, IConfiguration configuration, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+            _response = new ApiResponse();
+            _userManager = userManager;
+            _roleManager = roleManager;
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse>> GetProfileById(string userId)
+        {
+            ApplicationUser userFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == userId);
+            if (userFromDb == null)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.NotFound;
+                _response.ErrorMessages = "Có lỗi đã xảy ra, vui lòng thử lại sau!";
+                return BadRequest(_response);
+            }
+            _response.IsSuccess = true;
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.Data = userFromDb;
+            return Ok(_response);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ApiResponse>> UpdateProfileById(UpdateProfileDTO dto)
+        {
+            ApplicationUser userFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == dto.Id);
+            if (userFromDb == null)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.NotFound;
+                _response.ErrorMessages = "Có lỗi đã xảy ra, vui lòng thử lại sau!";
+                return BadRequest(_response);
+            }
+            userFromDb.UpdatedDate = DateTime.Now;
+            userFromDb.Address = dto.Address;
+            userFromDb.PhoneNumber = dto.PhoneNumber;
+            userFromDb.Fullname = dto.Fullname;
+            _db.Update(userFromDb);
+            _db.SaveChanges();
+            _response.IsSuccess = true;
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.Data = userFromDb;
+            return Ok(_response);
+        }
+    }
+}
---
+++ RailwayReservationAPI/RailwayReservationAPI/Models/Dto/UpdateProfileDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace RailwayReservationAPI.Models.Dto
+{
+    public class UpdateProfileDTO
+    {
+        public string Id { get; set; }
+        public string Fullname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
